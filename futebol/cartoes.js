b45f098c94f96d4c82d44962f0ad5991a4813e15
Willen Goulart <willen.goulart@credihome.com.br>
2022-06-24 07:18:26 -0300

    Corrigindo Path dos arquivos

--- futebol/cartoes.js
+++ futebol/cartoes.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 try {
-  const data = fs.readFileSync('C:/Users/wesle/OneDrive/Área de Trabalho/Projetinho/futebol/futebol/cartoes', 'utf8');
+  const data = fs.readFileSync('./cartoes', 'utf8');
   console.log(data);
 } catch (err) {
   console.error(err);
